30ffde39a15d5b56a3673b77dabc923899b28414
Matéo Journiac <journiacmateo@gmail.com>
2025-01-30 09:12:37 +0100

    commit href pages

--- includes/header.php
+++ includes/header.php
@@ -9,13 +9,13 @@ if (session_status() === PHP_SESSION_NONE) {
     <nav>
         <ul>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="products.php">Produits</a></li>
-            <li><a href="contact_support.php">Contact</a></li>
+            <li><a href="./pages/products.php">Produits</a></li>
+            <li><a href="./pages/contact_support.php">Contact</a></li>
             <?php if (isset($_SESSION['user_id'])): ?>
                 <li><a href="logout.php">Déconnexion</a></li>
             <?php else: ?>
-                <li><a href="login.php">Connexion</a></li>
-                <li><a href="register.php">Inscription</a></li>
+                <li><a href="./pages/login.php">Connexion</a></li>
+                <li><a href="./pages/register.php">Inscription</a></li>
             <?php endif; ?>
         </ul>
     </nav>
