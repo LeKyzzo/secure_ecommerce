ea27cb34d066484c0bc5203daa05c6099f864fd9
Matéo Journiac <journiacmateo@gmail.com>
2025-01-30 09:33:50 +0100

    commit passage chemin relatif

--- includes/header.php
+++ includes/header.php
@@ -9,13 +9,13 @@ if (session_status() === PHP_SESSION_NONE) {
     <nav>
         <ul>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="./pages/products.php">Produits</a></li>
-            <li><a href="./pages/contact.php">Contact</a></li>
+            <li><a href="pages/products.php">Produits</a></li>
+            <li><a href="pages/contact.php">Contact</a></li>
             <?php if (isset($_SESSION['user_id'])): ?>
                 <li><a href="logout.php">Déconnexion</a></li>
             <?php else: ?>
-                <li><a href="./pages/login.php">Connexion</a></li>
-                <li><a href="./pages/register.php">Inscription</a></li>
+                <li><a href="pages/login.php">Connexion</a></li>
+                <li><a href="pages/register.php">Inscription</a></li>
             <?php endif; ?>
         </ul>
     </nav>
