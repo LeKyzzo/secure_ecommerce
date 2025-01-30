a01243dcd4c78b76b756fabcc3c4cde6eee2a576
Matéo Journiac <journiacmateo@gmail.com>
2025-01-30 09:39:56 +0100

    commit correction chemin absolu

--- includes/header.php
+++ includes/header.php
@@ -9,13 +9,13 @@ if (session_status() === PHP_SESSION_NONE) {
     <nav>
         <ul>
             <li><a href="../index.php">Accueil</a></li>
-            <li><a href="efrei/secure_ecommerce/pages/products.php">Produits</a></li>
-            <li><a href="efrei/secure_ecommerce/pages/contact.php">Contact</a></li>
+            <li><a href="/efrei/secure_ecommerce/pages/products.php">Produits</a></li>
+            <li><a href="/efrei/secure_ecommerce/pages/contact.php">Contact</a></li>
             <?php if (isset($_SESSION['user_id'])): ?>
                 <li><a href="logout.php">Déconnexion</a></li>
             <?php else: ?>
-                <li><a href="efrei/secure_ecommerce/pages/login.php">Connexion</a></li>
-                <li><a href="efrei/secure_ecommerce/pages/register.php">Inscription</a></li>
+                <li><a href="/efrei/secure_ecommerce/pages/login.php">Connexion</a></li>
+                <li><a href="/efrei/secure_ecommerce/pages/register.php">Inscription</a></li>
             <?php endif; ?>
         </ul>
     </nav>
